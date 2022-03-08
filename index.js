d7eeda9830dd51ac056f8c66172bee0d3e87ce4d
ivbaldo <ivanbaldo1994@gmail.com>
2022-03-08 17:08:55 +0100

    API REST CRUD (con seguridad)

--- index.js
+++ index.js
@@ -3,17 +3,38 @@ const port = process.env.PORT || 3000;
 
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 const mongojs = require('mongojs');//importamos la base de datos mongojs
-
 const app = express();
 
 var db = mongojs("SD"); //Conectamos con la db SD
 var id = mongojs.ObjectId; //Funcion para convertir un id textual en un objetId
+//Declaraciones cors
+var allowCrossTokenHeader = (req, res, next) => {
+    res.header("Access-Control-Allow-Headers", "GET, POST, PUT, DELETE, OPTIONS");
+    return next();
+}
 
+var allowCrossTokenOrigin = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "token");
+    return next();
+};
+
+//Para autorizar las req
+var auth = (req, res, next) => {
+    if(req.headers.token === "password1234"){
+        return next();
+    }else{
+        return next(new Error("No autorizado"));
+    }
+};
 // Declaramos los middleware
 app.use(logger('dev')); // probar con: tiny, short, dev, common, combined
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.json())
+ //cors middlewares
+app.use(allowCrossTokenHeader);
+app.use(allowCrossTokenOrigin);
 
 //añadimos un tigger previo a las rutas para dar soporte a multiples colecciones
 app.param("coleccion", (req, res, next, coleccion) => {
@@ -50,7 +71,7 @@ app.get('/api/:coleccion/:id', (req, res, next) => {
     });
 });
 
-app.post('/api/:coleccion', (req,res,next) => {
+app.post('/api/:coleccion', auth, (req,res,next) => {
     const elemento = req.body;
 
     if(!elemento.nombre){
@@ -66,7 +87,7 @@ app.post('/api/:coleccion', (req,res,next) => {
     }
 });
 
-app.put('/api/:coleccion/:id', (req, res, next) => {
+app.put('/api/:coleccion/:id', auth, (req, res, next) => {
     let elementoId = req.params.id;
     let elementoNuevo = req.body;
     req.collection.update({_id: id(elementoId)},
@@ -76,10 +97,10 @@ app.put('/api/:coleccion/:id', (req, res, next) => {
     });
 });
 
-app.delete('/api/:colleccion/:id',(req,res,next) => {
+app.delete('/api/:coleccion/:id', auth,(req,res,next) => {
     let elementoId = req.params.id;
 
-    req.coleccion.remove({_id: id(elementoId)}, (err,resultado) => {
+    req.collection.remove({_id: id(elementoId)}, (err,resultado) => {
         if(err) return next(err);
         res.json(resultado);
     });
